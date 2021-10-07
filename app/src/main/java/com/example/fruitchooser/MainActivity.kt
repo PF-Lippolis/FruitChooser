4c60a2eb2b35d7103bef90b33c384d6622f680b0
P.F. Lippolis <pf.lippolis@gmail.com>
2021-10-07 17:32:56 +0200

    Add random fruit visualization

--- app/src/main/java/com/example/fruitchooser/MainActivity.kt
+++ app/src/main/java/com/example/fruitchooser/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.fruitchooser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import com.example.fruitchooser.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    val fruitArray = listOf(
+        R.drawable.apple,
+        R.drawable.bananas,
+        R.drawable.cherries,
+        R.drawable.grapes,
+        R.drawable.orange,
+        R.drawable.strawberry,
+        R.drawable.watermelon)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupButton()
+    }
+
+    private fun setupButton() {
+        binding.button.setOnClickListener {
+            val fruitSelection = Random.nextInt(fruitArray.size)
+            binding.image.visibility = View.INVISIBLE
+            binding.progressCircle.visibility = View.VISIBLE
+            binding.description.text = getString(R.string.loading_string)
+            binding.image.setImageResource(fruitArray[fruitSelection])
+            Handler(Looper.getMainLooper()).postDelayed({
+                binding.progressCircle.visibility = View.GONE
+                binding.image.visibility = View.VISIBLE
+                binding.description.text = getString(R.string.inviting_message)
+            }, 3000)
+        }
     }
 }
